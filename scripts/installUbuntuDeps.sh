39d322488b8e7cb043e224d90d3e289f5248dcef
guowei <guowzou@gmail.com>
2018-07-23 17:32:04 +0800

    Update installUbuntuDeps.sh
    Use libav v11.9

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -157,10 +157,10 @@ install_mediadeps(){
   sudo apt-get -qq install yasm libvpx. libx264.
   if [ -d $LIB_DIR ]; then
     cd $LIB_DIR
-    if [ ! -f ./v11.1.tar.gz ]; then
-      curl -O -L https://github.com/libav/libav/archive/v11.1.tar.gz
-      tar -zxvf v11.1.tar.gz
-      cd libav-11.1
+    if [ ! -f ./v11.9.tar.gz ]; then
+      curl -O -L https://github.com/libav/libav/archive/v11.9.tar.gz
+      tar -zxvf v11.9.tar.gz
+      cd libav-11.9
       PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig ./configure --prefix=$PREFIX_DIR --enable-shared --enable-gpl --enable-libvpx --enable-libx264 --enable-libopus --disable-doc
       make $FAST_MAKE -s V=0
       make install
@@ -180,10 +180,10 @@ install_mediadeps_nogpl(){
   sudo apt-get -qq install yasm libvpx.
   if [ -d $LIB_DIR ]; then
     cd $LIB_DIR
-    if [ ! -f ./v11.1.tar.gz ]; then
-      curl -O -L https://github.com/libav/libav/archive/v11.1.tar.gz
-      tar -zxvf v11.1.tar.gz
-      cd libav-11.1
+    if [ ! -f ./v11.9.tar.gz ]; then
+      curl -O -L https://github.com/libav/libav/archive/v11.9.tar.gz
+      tar -zxvf v11.9.tar.gz
+      cd libav-11.9
       PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig ./configure --prefix=$PREFIX_DIR --enable-shared --enable-gpl --enable-libvpx --enable-libopus --disable-doc
       make $FAST_MAKE -s V=0
       make install
