f7f7a2b159e8b2c6cd68cc4211423f0b00e0f595
zou.guowei <zou.guowei@immomo.com>
2018-07-24 16:32:30 +0800

    Using Host Name And SSL

--- scripts/licode_default.js
+++ scripts/licode_default.js
@@ -62,14 +62,14 @@ config.erizoController.networkinterface = ''; //default value: ''
 
 // This configuration is used by the clients to reach erizoController
 // Use '' to use the public IP address instead of a hostname
-config.erizoController.hostname = ''; //default value: ''
-config.erizoController.port = 8080; //default value: 8080
+config.erizoController.hostname = 'room.maytrue.net'; //default value: ''
+config.erizoController.port = 443; //default value: 8080
 // Use true if clients communicate with erizoController over SSL
 config.erizoController.ssl = true; //default value: false
 
 // This configuration is used by erizoController server to listen for connections
 // Use true if erizoController listens in HTTPS.
-config.erizoController.listen_ssl = true; //default value: false
+config.erizoController.listen_ssl = false; //default value: false
 config.erizoController.listen_port = 8080; //default value: 8080
 
 // Custom location for SSL certificates. Default located in /cert
